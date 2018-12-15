ef48cda4fc8f5407252e027a1f4646549ec26320
Axolotl <samuelmachat@gmail.com>
2018-12-14 23:44:52 -0500

    initial

---
+++ advent of code/02.js
@@ -0,0 +1,320 @@
+const ids = `uqyoeiznvmzrstpkgnsacjxwld
+uqloeizfvmbrstpkgndacjxwyd
+dqgoeizfbmbrstpkgnhacjxwld
+uqyjeizfvmbrstpygntacjxwld
+uqyoeizfhmbrstppbnhacjxwld
+uqyoeizfvmbrstpkschanjxwld
+uqyoeizfvtbrstpkgnmazjxwld
+uqyoeizfvmyrstpzgnhacjxsld
+udtoeizfvmbrttpkgnhacjxwld
+uqyneizfvmbratpognhacjxwld
+uqyoeizfvmbrstpkunhaajxnld
+uqyteizfvmbrstpkgnhzcnxwld
+hqyteuzfvmbrstpkgnhacjxwld
+uqyoeizyvmbrsdpkgnhacdxwld
+uqyoeizfibbrsepkgnhacjxwld
+uqxoeizfvmbrstpkgnhaxjjwld
+iqaoeizfvmbrstpkrnhacjxwld
+uqyoeizpvvbrstpkghhacjxwld
+aqyoeizfvmbrstpbgnhfcjxwld
+uqyonfzfvmbrstpkgnhacjgwld
+uqyoeizovmhrstpkgnhacjxwvd
+uqyoegzfvmbrstpkgnfacjxrld
+uqyoeizfvmbrstpjgnwacjxwdd
+uqyogiifumbrstpkgnhacjxwld
+uqyorizfvmbrstpkgnhaezxwld
+uqyoexdfvmbretpkgnhacjxwld
+uqyoeizfvmbrstpkgwhlcjxzld
+uqyoeibfvmbrstpggnhzcjxwld
+udyoeizfvmbrstppwnhacjxwld
+ufyoeizfvmhrstpkgnfacjxwld
+aqyoeftfvmbrstpkgnhacjxwld
+uhyoeivfvmbrstpkgnhadjxwld
+uqyoeizfvmbrstpkgmhacjxhud
+uqyfeizfvmbrstpkgnhkljxwld
+uqyoeibfvmbrstpkgbhazjxwld
+uqyceizfvnbrslpkgnhacjxwld
+uqyoeizfvmirstpkonhacaxwld
+fqyooizfvmbrstpkgnhacjxwlq
+uqydeizfvmbgstpugnhacjxwld
+uqyoiizfvmbrstpkgnhacjalld
+uqyivizfvmbrstpkgnhocjxwld
+uqyoeizfvmbrstpngnhjcjxwtd
+uqyoeizfvmbrstpkynhrcexwld
+ugmfeizfvmbrstpkgnhacjxwld
+uqyoeizrvmbrstpkwnhycjxwld
+uqyoeiyfvmbrstpkgnlacjxwwd
+unyoeizfvmbrstrkgnhadjxwld
+uqyoeizhvmbrstpkgnhacjawlz
+uqyoeazfvmbrstpwgnhacjxwlp
+uqyoeiznvmbastpkgnhaccxwld
+kqyoeisfvmbrstikgnhacjxwld
+uqyoeizfvlxrstpmgnhacjxwld
+uqyoeiofvmbrstpkgniacjxwll
+uqyoeizfvmbrswpegnhacixwld
+uqyoeizfvmarstlkgnhanjxwld
+uuyoeizfvmbrstykgnhacjxwbd
+uqyoeizfvmbpstxkgnhacjxwlu
+uqyoqizfvmbrsspkgnhacjpwld
+uqyoeizfvmzrstnkgnhacjgwld
+uqyoeizfvmbrstrcgnhacjxwud
+uqyoeizfvmbrstpkgnuacxxwsd
+cqyoeizfvmbrscpkgnhacjxwpd
+sqyoeizfaobrstpkgnhacjxwld
+uqyoeizfvmbrgtpkgnhacjxbwd
+uqyoeibhvmbrstpkgnhagjxwld
+uqyoeizfvcbrstpkgzhactxwld
+uqkoeizfvmbrstijgnhacjxwld
+uqroelzfvmbrstpkgmhacjxwld
+uqyoeizfvmbrsfpcgnhkcjxwld
+uqyoeizfumgrstpygnhacjxwld
+uqyozibfvmbrstpkgnhacjwwld
+uqyoeigfvmbrstpkgjharjxwld
+uqpoeizfvmbrstpkgnhacjxrcd
+uqyoeizfvmbrskokgnhqcjxwld
+uqyoeikfvmbrstpkgehacjxwle
+uqygeizfvhbrstpkgnqacjxwld
+lqyoeizfvmbrsvpkgnhacjuwld
+uqyjeizfvmbectpkgnhacjxwld
+unyoeizfvmbrstpkgnhzckxwld
+uqyoeizfvmbrstpkgmoacjxwlm
+uqfoeizfvmbrutpkgnhacjrwld
+uqyohizflnbrstpkgnhacjxwld
+uquoeizfembrstpkgniacjxwld
+uqyoeizfvmbrytpkgnhbcjkwld
+pqyoeizfvsbwstpkgnhacjxwld
+uqyoerzfvmbrstpkxntacjxwld
+uqyoeizfvmbritpkgnhacjqhld
+uqyoeisfvmbrstpkglhscjxwld
+hqyoeizfvmwrstpwgnhacjxwld
+uqyoeyzrvmbrstpkgnhacjxdld
+pqyoeozfombrstpkgnhacjxwld
+uwyosizfvmbrstpkgnhacjrwld
+uqyoegzfvmbrstekbnhacjxwld
+uqyoeinfvmirstpkgdhacjxwld
+uqyoeizfvmbistpkgnobcjtwld
+uqyoeikfvmbrsepkgnhycjxwld
+qqyoeiafvmbrstpkgnhacjxold
+uqbooizfvmbrstpkgnhacjxwdd
+uquoeizfvmbrswpkgnhatjxwld
+tqyoejzfvmbrstpkgnhacjxwlv
+yqyoeizfvmbrsttkgnhakjxwld
+usyoeizfvmbfstpkgnhkcjxwld
+gqyocizfvmbrstpkgnhacjbwld
+mqyoeizfvmbrstpkgnhacjzzld
+uqyoeczfvmbrotpkgnhacjxwsd
+jqyoeizfvmkrstpkgnhacaxwld
+uqyoeirfvmblstpkgnhaijxwld
+uqpoeirfvmbrstpkgbhacjxwld
+uqyoiizcvmbrsipkgnhacjxwld
+uqyoeizfvmbrstekgnhayjywld
+uqyoeizpvmnrstpkgnhacjxwcd
+eqyoaizfvmbrstpkgnhacjzwld
+uqyubizfvmbrltpkgnhacjxwld
+kqmoeizfvmbrstbkgnhacjxwld
+uqyoeizfvmkrstpvgnhahjxwld
+uqyoeizfrmbrsjpkgchacjxwld
+uqyoeizfvmbrstfkgnhacbxwlx
+uqyoeizfvmbrstpkbngayjxwld
+uqyorizfvmbrstpkgghacjxwle
+oqyoeizfvpkrstpkgnhacjxwld
+uqyoexzfvmbrscpkgnhacjxwlq
+uqkoyilfvmbrstpkgnhacjxwld
+uqyotizfamnrstpkgnhacjxwld
+uqnoeizfvmbrstpkgnhacdfwld
+uqyoeizffmbrstqkgnhacuxwld
+uqyoeizfvmbrzhpkgnhqcjxwld
+uqyzeizfvmbrsnpkgnracjxwld
+jhyoeizfvhbrstpkgnhacjxwld
+uqyohizfvmbrstpkgnqhcjxwld
+uqyleizfgmbrstlkgnhacjxwld
+ucyoeizzvmbrstgkgnhacjxwld
+uqyoeizqvmbrstbkgahacjxwld
+uqyseizpvkbrstpkgnhacjxwld
+uqyobizfvmirstpkgnhachxwld
+uqyoiizfvmbrstpkgnhacwxzld
+uqyoeizfvmbrstpkgnhafoxsld
+pqyoeizfvmbrstpkgnhaijxwle
+uqyoeizfvubrstpkgneacjxcld
+uqyoeigfumbrstpknnhacjxwld
+uqyoeiffvmbrstwkgnhacjxxld
+uqooeizfvmbrstpkgnhacjxilv
+uqyoeizfvmbrstpkgnhaljirld
+uqyoeizfvmbistpkgnoacjtwld
+uqyogizfvmlustpkgnhacjxwld
+uqyoeizfvmufjtpkgnhacjxwld
+uqyoeixfvmbrstpugwhacjxwld
+uqyoeizfrzbrsjpkgnhacjxwld
+uqyoeizfvmbbsqpkgzhacjxwld
+uqyoeizovqfrstpkgnhacjxwld
+uqyieizfvmbrsjpkgnhacjxwzd
+uqdoeizfvmbrstnkgnqacjxwld
+usyoohzfvmbrstpkgnhacjxwld
+uqyoeicfvmbrstpkgnhacgxwtd
+uqyoeizfvmbrstpkgkhbcjxold
+fwyoeizfvmbrstpkgnhkcjxwld
+oqyoeizfvmbrstpkenhacjjwld
+uqyoeizfvmbrttpkgjhacjhwld
+uqyoeituvmbrstpkgohacjxwld
+uqyoeizfvekrslpkgnhacjxwld
+upyoeizfvmbrstpkgnhjcjxwlb
+uqyoeiwfvmbrstpkgshacjmwld
+uqyceizfvmbrstpkgnpazjxwld
+aqyoeizfvmbrstpkgntacjxwed
+uqyosizfvmbrsapkgphacjxwld
+uqyoeizfvmbysipkgnhacjxwls
+uqyoeicfvmbgstpkgnhacjxwjd
+uqyoxizfvmbrsggkgnhacjxwld
+uqyoeizfvbbrhtplgnhacjxwld
+usyoeizfvmbrstvkgnhacjxwlo
+uqyleizfvmbrstpkgnhacjxwac
+uqyoeisfvmbrstpfgnhacjxwdd
+uqyoeizwvmbvstpkgnhacjxhld
+uqyoeczfvmbrstpkgnhecjxwlv
+uqyoeizfvmbrstpkgnhacjueid
+uqyoeisfvmbrstpkgnhaxjxwzd
+uqyoeizfvmbrstpkgnhicjmyld
+ugygeizfvmbrmtpkgnhacjxwld
+uoyoeiftvmbrstpkgnhacjxwld
+uqyoeinfvmbrstpkenhacjxwbd
+xqyoeiofvmbrstpkgnhacjxwlt
+uqyoeizfvnbrsipkgnhfcjxwld
+uqyteizfvmbrstpngmhacjxwld
+uqyoeijfvmbrstpdgnhacjcwld
+uqyoeizfjmbrstpkgntaclxwld
+uqyaeizfvmbrstpkgnhajjxwkd
+uqyoeizfvmbraqpkgnhacjxwlv
+uqyocizfvmbrstpkqnhrcjxwld
+uqyoqizfvmbrdtpkenhacjxwld
+uqyoeicfvwbrstpkknhacjxwld
+uqyoeizfvmbrstpktnhacjxxlz
+uqyoeizfvmbhstpkgnhacjmwgd
+uqyoeizfvmorskpkgnhncjxwld
+nqyxeezfvmbrstpkgnhacjxwld
+uqyoeitfvmbrstpksnhicjxwld
+uqyoeizfmmwrstpkgnhacjxwud
+uqyoeizfvmmrstpkgnhvcjxwlk
+uqyoeizfvmbrstpkgihacjxblz
+uqnoeizfvmwrstpkgnhaujxwld
+uqyoeizfvlbrsrpkgnhacjxwpd
+uqyoeizfvmbwstpkgnhvcjxwdd
+uqyoeixfvlbrstpkgxhacjxwld
+uqyoeizfvmbjstpmgnhacjxmld
+uqyoeizyvhbrstpkgnhacjxwzd
+uqyhepzfvmbrstpkghhacjxwld
+uqyoetzfvmbrstpkghsacjxwld
+uqyoeizfvmbrbnpkgnuacjxwld
+uqyokizfvmbrstpkgnhacqiwld
+uqyoeizfvmbrstpkgqhacagwld
+uqyoeizfvnbvstckgnhacjxwld
+uqyoeizfbmbhstpkjnhacjxwld
+uqyoeixfvmbrstpkgnegcjxwld
+uqyoeizfvmbrstpkshlacjxwld
+uqyoeisfhmbrsnpkgnhacjxwld
+uqyoekzfvmbrstpkgnhadjxsld
+uqyoeizfzmyrstckgnhacjxwld
+uqyoeizpvmbrztpkenhacjxwld
+uqyopizffmbrstpkgnhacjxuld
+uqyoeizfvzhrstpkgnhscjxwld
+uqyoeizfvlbrstskvnhacjxwld
+uqyohizfvmbrstekgnhacaxwld
+uqlodizfvmbastpkgnhacjxwld
+uqyoeizfvubrstpbgnhacjvwld
+uqnoupzfvmbrstpkgnhacjxwld
+tqzoeizfvmbrsdpkgnhacjxwld
+pqyobizfvmbrstphgnhacjxwld
+uqyweizfvkbrstpkguhacjxwld
+uqyoexzfvmbrstzktnhacjxwld
+cqyoeizfvmbrstpkgnhacvxfld
+uqyoeiefvmbrytpmgnhacjxwld
+uqyoeizfvmbhstpkgnhmcjpwld
+uqyoeizfvnbrstpkgnhaczrwld
+uqyoeibfvnbrstpkgnhacjxvld
+uqyoeizfvebrsypygnhacjxwld
+uqyoeikfnmbrstukgnhacjxwld
+uqyoeizfvmbrsfpkgnhfcdxwld
+uqyoeizfvmmrstpnbnhacjxwld
+uqyoeizfvmbrztpegnhacjxwrd
+pqyoeizfvmbrqtukgnhacjxwld
+uqyoeizfvmbbstpkgahacjxpld
+uqsoeizfvmbpstpkgnhatjxwld
+uqyodizfvmbrsfpkgnhgcjxwld
+uuyoeizfvmbnstpkgnhacjxbld
+gqyoeizfvmbrstpkgnhacjqwcd
+uqyoeizfvmyrotkkgnhacjxwld
+uqyoeizfvmabstpkgnhacjawld
+uqyoeizfvmbrstpkglhaszxwld
+ukyoeizfvmbrstpkgnhacjxtfd
+uqeoeizfvmbrstpkgnhtnjxwld
+uqyouizfvmnrstpkgxhacjxwld
+ugyoeizfvmbrsupygnhacjxwld`
+
+const idsArray = ids.split('\n')
+// let doubles = 0
+// let triples = 0
+
+// const findVal = item => {
+//   let letters = {}
+
+//   for (let i = 0; i < item.length; i++) {
+//     if (!letters[item[i]]) {
+//       letters[item[i]] = 1
+//     } else {
+//       letters[item[i]] += 1
+//     }
+//   }
+
+//   let twos = false
+//   let threes = false
+//   Object.keys(letters).forEach(key => {
+//     if (letters[key] === 2) {
+//       twos = true
+//     } else if (letters[key] > 2) {
+//       threes = true
+//     }
+//   })
+
+//   if (twos) {
+//     doubles++
+//   }
+//   if (threes) {
+//     triples++
+//   }
+// }
+
+// idsArray.forEach(item => findVal(item))
+// // console.log(doubles * triples)
+
+// for x in id_array:
+// 	for y in id_array:
+//     	differential = 0
+//          for i in range(len(x)):
+//             if x[i] != y[i]:
+//                  differential += 1
+//         if differential == 1:
+//             final_arr = []
+//             for i in range(len(x)):
+//                  if x[i] == y[i]:
+//                     final_arr.append(x[i])
+// 				print ''.join(final_arr)
+
+for (let i = 0; i < idsArray.length; i++) {
+  for (let j = 0; j < idsArray.length; j++) {
+    let difference = 0
+    for (let k = 0; k < idsArray[i].length; k++) {
+      if (idsArray[i][k] !== idsArray[j][k]) {
+        difference++
+      }
+    }
+    if (difference === 1) {
+      const result = []
+      for (let l = 0; l < idsArray[i].length; l++) {
+        if (idsArray[i][l] === idsArray[j][l]) {
+          result.push(idsArray[i][l])
+        }
+      }
+      console.log(result.join(''))
+      break
+    }
+  }
+}
